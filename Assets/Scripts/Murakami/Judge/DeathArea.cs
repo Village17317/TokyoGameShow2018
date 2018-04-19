9c8f6f45ebf9894ed426569f378e9da52fdc2375
kmurakami <village0427@gmail.com>
2018-04-19 10:42:58 +0900

    restart時にwaitTimeを設けました

--- Assets/Scripts/Murakami/Judge/DeathArea.cs
+++ Assets/Scripts/Murakami/Judge/DeathArea.cs
@@ -14,10 +14,12 @@ namespace Village {
     public class DeathArea : MonoBehaviour {
 
         [SerializeField] private StartFlagment startFlagment;
-
+        [SerializeField] private Rigidbody2D player2DRigid;
         private void OnTriggerEnter2D(Collider2D collision) {
             if(collision.tag == "Player") {
+                player2DRigid.velocity = Vector3.zero;
                 startFlagment.ReSpawn();
+                GameMaster.getInstance.SetGameMode(GameMaster.GameMode.GameReStart);
                 GameMaster.getInstance.DeadCountUp();//MainManagerで死んだ回数をカウントする
             }
         }

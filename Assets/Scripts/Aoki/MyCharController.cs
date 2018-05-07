d8b8e584aa01898f9b5670292a6bab6e12282dfc
NI INI <xnc98457@gmail.com>
2018-05-07 13:51:30 +0900

    目の前の穴を飛び越えるコードを追加

--- Assets/Scripts/Aoki/MyCharController.cs
+++ Assets/Scripts/Aoki/MyCharController.cs
@@ -14,7 +14,7 @@ namespace INI {
 	public class MyCharController : Village.Inheritor {
 
         [SerializeField]
-        private bool reverse = false, inShadow = false, isStop = false, isGround = false;
+        private bool reverse = false, inShadow = false, isStop = false, isGround = false, nearJudge = false, farJudge = false, jump = false;
 
         [SerializeField]
         private Rigidbody2D myCharRb;
@@ -22,18 +22,15 @@ namespace INI {
         [SerializeField]
         private float mvSpd = 1.0f;
 
+        [SerializeField]
+        private Vector2 jmpSpd = Vector2.zero;
+
         public override void FixedRun()
 		{
             if (Village.GameMaster.getInstance.GetGameMode == Village.GameMaster.GameMode.Game)
             {
-                //if (Input.GetButtonDown("Button_B"))
-                //{
-                //    isStop = !isStop;
-                //}
-
-                if (!isStop)
+                if (isGround)
                 {
-
                     if (inShadow)
                     {
                         Up();
@@ -41,8 +38,13 @@ namespace INI {
                     else
                     {
                         Walk();
+
+                        if ((!nearJudge && farJudge) || jump)
+                        {
+                            myCharRb.constraints = RigidbodyConstraints2D.None;
+                            myCharRb.AddForce(jmpSpd);
+                        }
                     }
-                    
                 }
             }
             else if (Village.GameMaster.getInstance.GetGameMode == Village.GameMaster.GameMode.Pause)
@@ -58,46 +60,25 @@ namespace INI {
             this.transform.Translate(mvSpd, 0, 0);
         }
 
-        private void Down()
-        {
-            this.transform.localEulerAngles = new Vector3(0, 0, 0);
-            this.transform.Translate(mvSpd, -mvSpd, 0);
-        }
-
         private void Up()
         {
             this.transform.localEulerAngles = new Vector3(0, 0, 0);
             this.transform.Translate(0, mvSpd, 0);
-        }
-
-        private void Stop()
-        {
-            this.transform.Translate(Vector3.zero);
         }
         
         /// <summary>
         /// 接地判定を受け取るメソッド。真は接地、偽は非接地に対応。
         /// </summary>
         /// <param name="gnd"></param>
-        public void IsGround(bool gnd, bool rvs)
+        public void IsGround(bool gnd)
         {
             isGround = gnd;
-            reverse = rvs;
 
             if (gnd)
             {
                 //myCharRb.simulated = false;
                 myCharRb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-
-                if (rvs)
-                {
-                    this.transform.localEulerAngles += new Vector3(0, 180, 0);
-
-                    if (this.transform.localEulerAngles.y >= 360)
-                    {
-                        this.transform.localEulerAngles = new Vector3(0, 0, 0);
-                    }
-                }
+                
             }
             else if (!gnd)
             {
@@ -105,7 +86,53 @@ namespace INI {
                 myCharRb.constraints = RigidbodyConstraints2D.FreezeRotation;
             }
         }
-        
+
+        /// <summary>
+        /// 反転判定を受け取るメソッド。真の時に反転する。
+        /// </summary>
+        /// <param name="rvs"></param>
+        public void Reverse(bool rvs)
+        {
+            reverse = rvs;
+
+            if (rvs)
+            {
+                this.transform.localEulerAngles += new Vector3(0, 180, 0);
+
+                if (this.transform.localEulerAngles.y >= 360)
+                {
+                    this.transform.localEulerAngles = new Vector3(0, 0, 0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 目の前の段差の判定を受け取りローカル変数にセットするクラス。
+        /// </summary>
+        /// <param name="jmp"></param>
+        public void SetJump(bool jmp)
+        {
+            jump = jmp;
+        }
+
+        /// <summary>
+        /// 近距離の足場の判定を受け取りローカル変数にセットするクラス。
+        /// </summary>
+        /// <param name="near"></param>
+        public void SetNear(bool near)
+        {
+            nearJudge = near;
+        }
+
+        /// <summary>
+        /// 遠距離の足場の判定を受け取りローカル変数にセットするクラス。
+        /// </summary>
+        /// <param name="far"></param>
+        public void SetFar(bool far)
+        {
+            farJudge = far;
+        }
+
         /// <summary>
         /// 影の中に居る場合にこのメソッドに真を渡す。
         /// </summary>

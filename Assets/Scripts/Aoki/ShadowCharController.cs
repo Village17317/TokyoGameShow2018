6c070646e0e5424f950807949dc2ebe2e709d3d3
NI INI <xnc98457@gmail.com>
2018-05-11 11:23:05 +0900

    テスト用ステージを複製 MyCharController は ShadowCharController に置き換え、MyCharController はoldフォルダ内に移動

---
+++ Assets/Scripts/Aoki/ShadowCharController.cs
@@ -0,0 +1,27 @@
+﻿/*
+*	作成者	：青木仁志
+*	機能	：キャラクターの自動操作
+*	作成	：2018/05/11
+*	更新	：2018/05/11
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace INI
+{
+    public class ShadowCharController : Village.Inheritor
+    {
+        public float walkSpeed = 10;
+        public Vector3 jumpForce;
+
+        public override void FixedRun()
+        {
+            if (Village.GameMaster.getInstance.GetGameMode == Village.GameMaster.GameMode.Game)
+            {
+
+            }
+        }
+    }
+}

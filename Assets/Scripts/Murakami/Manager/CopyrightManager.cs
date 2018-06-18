c5917032e53f812c45a0874dd48d92824eb5c8c9
kmurakami <village0427@gmail.com>
2018-06-18 11:21:53 +0900

    ステージ変更

---
+++ Assets/Scripts/Murakami/Manager/CopyrightManager.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *	作成者     :
+ *	機能説明   :
+ * 	初回作成日 :
+ *	最終更新日 :
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Village {
+
+    public class CopyrightManager: MonoBehaviour {
+
+        [SerializeField] private string titleSceneName;
+        [SerializeField] private float maxTime = 2.0f;
+
+        private float time = 0;
+        private bool isOnce = false;
+
+        private void Update() {
+            time += Time.deltaTime;
+            if(time >= maxTime && !isOnce) {
+                isOnce = true;
+                SceneManager.LoadScene(titleSceneName);
+            }
+        }
+
+    }
+}

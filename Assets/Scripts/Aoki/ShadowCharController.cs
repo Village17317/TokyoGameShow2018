e0f981038311487ad02acd9aaf949c232200b8ec
NI INI <xnc98457@gmail.com>
2018-06-19 10:16:31 +0900

    各スクリプトにコメントを追加、一部余分な行を削除

--- Assets/Scripts/Aoki/ShadowCharController.cs
+++ Assets/Scripts/Aoki/ShadowCharController.cs
@@ -2,7 +2,7 @@
 *	作成者	：青木仁志
 *	機能	：キャラクターの自動操作
 *	作成	：2018/05/11
-*	更新	：2018/06/15
+*	更新	：2018/06/19
 */
 
 using System.Collections;
@@ -13,25 +13,33 @@ namespace INI
 {
     public class ShadowCharController : Village.Inheritor
     {
+        // 移動速度、正面に飛ばすRayの距離、足元に飛ばすRayの距離、穴・段差判定用に飛ばすRayの距離
         public float walkSpeed = 0.1f, fwRayDistance = 10f, dwRayDistance = 10.8f, sfRayDistance;
 
+        // ジャンプ時の力、正面に飛ばすRayの原点、穴・段差判定用に飛ばすRayの原点
         public Vector3 jumpForce, fwRayOrigin, sfRayOrigin;
 
+        // 影のキャラのRigidbody
         [SerializeField]
         private Rigidbody shadowCharRb;
 
+        // 接地フラグ
         [SerializeField]
-        private bool grounded = false, eventAnim = false;
+        private bool grounded = false;
 
+        // アニメーションの状態を示すenum
         protected enum State
         {
             STOP,
             WALK,
             JUMP
         }
+
+        // State型のenumの宣言
         [SerializeField]
         protected State state = State.STOP;
 
+        // 影のキャラのアニメーターを持つ
         public Animator shadowChar_Animator;
 
         public override void FixedRun()
@@ -74,6 +82,7 @@ namespace INI
 
             RaycastHit hit;
 
+            // デバッグ用にRayを可視化
             Debug.DrawRay(gndRay.origin, gndRay.direction * dwRayDistance, Color.red);
 
             if (Physics.Raycast(gndRay, out hit, dwRayDistance))
@@ -91,7 +100,7 @@ namespace INI
         }
 
         /// <summary>
-        /// 登り段差判定用
+        /// 段差判定（横）
         /// </summary>
         private void ForwardRayCast()
         {
@@ -99,6 +108,7 @@ namespace INI
 
             RaycastHit hit;
 
+            // デバッグ用にRayを可視化
             Debug.DrawRay(fwdRay.origin, fwdRay.direction * fwRayDistance, Color.red);
 
             if (Physics.Raycast(fwdRay, out hit, fwRayDistance))
@@ -114,12 +124,16 @@ namespace INI
             }
         }
 
+        /// <summary>
+        /// 穴・段差判定（前方下）
+        /// </summary>
         private void SafetyRayCast()
         {
             Ray sfRay = new Ray(this.transform.position + sfRayOrigin, Vector3.down);
 
             RaycastHit hit;
-            
+
+            // デバッグ用にRayを可視化
             Debug.DrawRay(sfRay.origin, sfRay.direction * sfRayDistance, Color.blue);
 
             if (Physics.Raycast(sfRay, out hit, sfRayDistance))
@@ -137,7 +151,6 @@ namespace INI
                 if (grounded && state != State.JUMP && state != State.STOP)
                 {
                     state = State.STOP;
-                    Debug.Log(state);
                 }
             }
         }
@@ -156,30 +169,25 @@ namespace INI
         private void Jump()
         {
             shadowCharRb.AddForce(jumpForce);
-            Debug.Log("Jump called");
         }
 
+        /// <summary>
+        /// ゲームの状態、及びState型enumの中身に応じてアニメーションを切り替える（プレイヤー用）
+        /// </summary>
         protected virtual void PlayerAnimator()
         {
             if (Village.GameMaster.getInstance.GetGameMode == Village.GameMaster.GameMode.GameClear)
             {
-                if (!eventAnim)
-                {
-                    eventAnim = true;
-                    shadowChar_Animator.Play("Clear");
-                }
+                shadowChar_Animator.Play("Clear");
                 return;
             }
 
             if (Village.GameMaster.getInstance.GetGameMode == Village.GameMaster.GameMode.GameOver)
             {
-                if (!eventAnim)
-                {
-                    eventAnim = true;
-                    shadowChar_Animator.Play("Down");
-                }
+                shadowChar_Animator.Play("Down");
                 return;
             }
+
             switch (state)
             {
                 case State.STOP:
